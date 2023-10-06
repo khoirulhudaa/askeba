ffaefabb3fe57d8748e525e976bf9e57ca3df9e9
valclassdevelop <developervalclass.com>
2023-10-06 10:19:50 +0700

    Web export at Askeba

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const slider = document.querySelector('.slider');
+let currentIndex = 0;
+const numSlides = 3; // Jumlah gambar
+
+function nextSlide() {
+    currentIndex++;
+    if (currentIndex >= numSlides) {
+        currentIndex = 0;
+    }
+    moveSlider();
+}
+
+function moveSlider() {
+    const slideWidth = slider.clientWidth;
+    slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+setInterval(nextSlide, 3000); // Mengganti slide otomatis setiap 3 detik
+
+moveSlider(); // Memastikan slider dimulai dari gambar pertama
